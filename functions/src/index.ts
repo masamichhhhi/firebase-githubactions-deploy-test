a85f71a1dfbf8ab61501db9a6c069d3ad38862e1
飯盛正慶 <masamichhhi@gmail.com>
2021-05-06 03:00:35 +0900

    add myNam

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,9 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+export const myName = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("masamichi");
+});
